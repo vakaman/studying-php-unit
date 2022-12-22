9364a57974623cf269258646db8ee61a4753c95d
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2022-12-22 16:30:33 -0300

    refact: adjust test runner to be more readable

--- class_1/runTests.php
+++ class_1/runTests.php
@@ -2,5 +2,37 @@
 
 include 'autoloader.php';
 
-$discountCalculatorTest = new DiscountCalculatorTest();
-$discountCalculatorTest->ShoudApplyWhenValueIsAboveTheMinimum();
+function classDoesNotEndsWithPrefixTest($file)
+{
+    return substr($file->getFilename(), -8) !== 'Test.php';
+}
+
+function methodDoesNotEndsWithPrefixTest($method)
+{
+    return substr($method, -4) !== 'Test';
+}
+
+function executeAllPublicMethods($file)
+{
+    $className = substr($file->getFilename(), 0, -4);
+    $testClass = new $className();
+    $classMethods = get_class_methods($testClass);
+
+    foreach ($classMethods as $method) {
+
+        if (methodDoesNotEndsWithPrefixTest($method)) {
+            continue;
+        }
+
+        $testClass->$method();
+    }
+}
+
+foreach (new DirectoryIterator(__DIR__) as $file) {
+
+    if (classDoesNotEndsWithPrefixTest($file)) {
+        continue;
+    }
+
+    executeAllPublicMethods($file);
+}
