492e471e100bd4cce8bfdf57eb4755b53566b857
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-02-14 12:31:32 -0300

    feat: example of dataProvider

--- class_2/src/OrderBundle/Test/Validators/NotEmptyValidatorTest.php
+++ class_2/src/OrderBundle/Test/Validators/NotEmptyValidatorTest.php
@@ -15,7 +15,7 @@ class NotEmptyValidatorTest extends TestCase
 
         $this->assertFalse($isValid);
     }
-    
+
     public function test_should_be_valid_when_value_is_not_empty()
     {
         $notEmptyValidator = new NotEmptyValidator('duvidei');
@@ -23,5 +23,21 @@ class NotEmptyValidatorTest extends TestCase
         $isValid = $notEmptyValidator->isValid();
 
         $this->assertTrue($isValid);
+    }
+
+    public function test_is_valid()
+    {
+        $dataProvider = [
+            '' => false,
+            'duvidei' => true
+        ];
+
+        foreach ($dataProvider as $value => $expectedValue) {
+            $notEmptyValidator = new NotEmptyValidator($value);
+
+            $isValid = $notEmptyValidator->isValid();
+
+            $this->assertEquals($expectedValue, $isValid);
+        }
     }
 }
