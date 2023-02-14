0393536fa88918b025baaa7d216f56615021dfd5
Maicol Kaiser <maicolkaiser.oliveira@gmail.com>
2023-02-14 12:24:50 -0300

    feat: add NotEmptyValidatorTest

---
+++ class_2/src/OrderBundle/Test/Validators/NotEmptyValidatorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\OrderBundle\Test\Validator;
+
+use Src\OrderBundle\Validators\NotEmptyValidator;
+use PHPUnit\Framework\TestCase;
+
+class NotEmptyValidatorTest extends TestCase
+{
+    public function test_should_not_be_valid_when_value_is_empty()
+    {
+        $notEmptyValidator = new NotEmptyValidator('');
+
+        $isValid = $notEmptyValidator->isValid();
+
+        $this->assertFalse($isValid);
+    }
+    
+    public function test_should_be_valid_when_value_is_not_empty()
+    {
+        $notEmptyValidator = new NotEmptyValidator('duvidei');
+
+        $isValid = $notEmptyValidator->isValid();
+
+        $this->assertTrue($isValid);
+    }
+}
